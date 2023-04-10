c69f42761fbccbf85e971acefe150463176d95ae
Maxime <maxime.briand@cbp-group.com>
2023-04-10 20:13:05 +0200

    feat(config): add capacitor configuration to have hot reload

--- apps/golden-hour/src/app/app.component.ts
+++ apps/golden-hour/src/app/app.component.ts
@@ -1,15 +1,16 @@
-import { Component } from "@angular/core";
+import {Component, signal} from '@angular/core';
 import { RouterModule } from "@angular/router";
 import { IonicModule } from "@ionic/angular";
-import { NxWelcomeComponent } from "./nx-welcome.component";
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule, IonicModule],
+  imports: [ RouterModule, IonicModule],
   selector: "native-apps-root",
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.scss"],
 })
 export class AppComponent {
   title = "golden-hour";
+
+  public signal = signal(19)
 }
--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,7 +4,10 @@ const config: CapacitorConfig = {
   appId: 'fr.maximebriand.goldenapp',
   appName: 'golden-hour',
   webDir: 'dist/apps/golden-hour',
-  bundledWebRuntime: false
+  bundledWebRuntime: false,
+  "server": {
+    "url": "http://localhost:4200"
+  }
 };
 
 export default config;
